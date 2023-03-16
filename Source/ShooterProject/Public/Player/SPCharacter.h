9c5153dce78c50510f0d70e5582fc42d9426c061
MykolaBulyhaKG <62252443+Sykess3@users.noreply.github.com>
2023-03-16 11:44:52 +0200

    added SPCharacter, SPPlayerController and relevant bluprints

---
+++ Source/ShooterProject/Public/Player/SPCharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ALSCharacter.h"
+#include "SPCharacter.generated.h"
+
+/**
+ * 
+ */
+class AALSCharacter;
+
+UCLASS()
+class SHOOTERPROJECT_API ASPCharacter : public AALSCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ShooterProject/Public/Player/SPPlayerController.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ALSPlayerController.h"
+#include "SPPlayerController.generated.h"
+
+class AALSBaseCharacter;
+class UInputMappingContext;
+
+/**
+ * 
+ */
+
+UCLASS()
+class SHOOTERPROJECT_API ASPPlayerController : public AALSPlayerController
+{
+	GENERATED_BODY()
+
+	
+	
+};
+
